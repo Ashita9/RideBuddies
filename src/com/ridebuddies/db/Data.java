9158751741b99da336bac4bf3dc516084aa8ca03
Ashita9 <ashita98@outlook.com>
2019-11-10 16:37:04 +0530

    update

---
+++ src/com/ridebuddies/db/Data.java
@@ -0,0 +1,9 @@
+package com.ridebuddies.db;
+
+public interface Data {
+
+	String user = "remote";
+	String password = "redhat@123";
+	String url = "jdbc:mysql://34.67.104.88:3306/ridebuddies?useSSL=false";
+	String driver = "com.mysql.jdbc.Driver";
+}
